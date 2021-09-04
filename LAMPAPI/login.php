79f862568556b706cef6da22b185d07fdbedd753
kaylamarc <kkcoons@aol.com>
2021-09-03 21:55:25 -0400

    Revert "update login json"
    This reverts commit 9078b1d609679b608f0c99edf4082ce24cddb0e0.

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -26,7 +26,7 @@
             $stmt->bind_param("ss", $inData["email"], $inData["password"]);
             $stmt->execute();
 
-            returnSuccess($inData["id"]);
+            returnWithInfo($row['id'], $row['firstname'], $row['lastname']);
         }
         else {
             returnWithError("Login failed.");
@@ -55,8 +55,9 @@
         sendResultInfoAsJson($retValue);
     }
 
-    // return json stating that the login was successful
-    function returnSuccess($id) {
-        sendResultInfoAsJson('{"status": "success", "id": "' . $id . '", "message": "Login Successful."}');
-    }
+    // 
+    function returnWithInfo($id, $firstName, $lastName) {
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '"}';
+		sendResultInfoAsJson($retValue);
+	}
 ?>
